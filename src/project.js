291695b6dce1b2f04e3507ca02c3b5f159ce311f
Andrew Erasmus <andrew.r.eras@gmail.com>
2024-12-09 12:56:24 +0200

    Add project module

---
+++ src/project.js
@@ -0,0 +1,9 @@
+let project = function (titleIn, descrIn) {
+  let title = titleIn;
+  let description = descrIn;
+  let todos = [];
+
+  return { title, description, todos };
+};
+
+export { project };
