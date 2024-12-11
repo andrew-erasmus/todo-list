7ae7c61f5443a13cdc24250b2632d50cb6349b10
Andrew Erasmus <andrew.r.eras@gmail.com>
2024-12-11 12:53:15 +0200

    Add layout and styling for todo list

--- src/index.js
+++ src/index.js
@@ -3,11 +3,6 @@ import { createTodo } from "./listItem.js";
 
 let projectList = [];
 let projectId = 0;
-addProject(projectId++, "Home", "Main list of to dos");
-addProject(projectId++, "2", "2 list of to dos");
-addTodo(projectList[0], "New", "", "", "", "");
-addTodo(projectList[0], "New", "", "", "", "");
-addTodo(projectList[0], "New", "", "", "", "");
 
 function addProject(id, title, description) {
   let projectNew = createProject(id, title, description);
@@ -34,6 +29,3 @@ function addTodo(project, title, description, dueDate, priority, notes) {
 function removeTodo(todoId, project) {
   project.todos.splice(todoId, 1);
 }
-
-removeTodo(1, projectList[0]);
-console.log(projectList);
